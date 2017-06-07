d0b9a81d76c988cc55ddf1821d7a82176c346c27
PaxWen <wolw0@mail.ru>
2017-06-07 13:39:57 +0400

    Добавленны 3 и 4 "ДЗ", 4 недоделано 2, 3 задание

---
+++ Task_03/HMT_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMT_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Написать программу, которая заменяет все положительные элементы в трёхмерном массиве на нули. 
+             * Число элементов в массиве и их тип определяются разработчиком.
+             * */
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Random rnd = new Random();
+            const int N = 4;
+            int[,,] Arr = new int[N, N, N];
+            while (true)
+            {
+                Console.Clear();
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        for (int k = 0; k < N; k++)
+                        {
+                            Arr[i, j, k] = rnd.Next(-10, 10);
+                        }
+                    }
+                }
+                outArray(Arr, N);
+                for (int i = 0; i < N; i++)
+                {
+                    for (int j = 0; j < N; j++)
+                    {
+                        for (int k = 0; k < N; k++)
+                        {
+                            if (Arr[i, j, k] > 0)
+                            {
+                                Arr[i, j, k] = 0;
+                            }
+                        }
+                    }
+                }
+                outArray(Arr, N);
+                Console.Write("Для выхода нажмите \'Enter\'...");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+        }
+        static void outArray(int [,,] Arr,int N)
+        {
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("{0}",i);
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        Console.Write("{0,4}",Arr[i,j,k]);
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ Task_04/HMT_01/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMT_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Написать программу, которая определяет среднюю длину слова во введенной текстовой строке.
+             *  Учесть, что символы пунктуации на длину слов влиять не должны. 
+             *  Регулярные выражения не использовать. И не пытайтесь прописать все ручками.
+             *   Используйте стандартные методы класса String. 
+             */
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            string sentence;
+            string text;
+            int sum;
+            int k;//счётчик слов
+            while (true)
+            {
+                Console.Clear();
+                text = "";
+                sum = 0;
+                k = 0;
+                Console.Write("Введите текст: ");
+                sentence = Console.ReadLine();
+                sentence += " ";
+                sentence.ToLower();
+                for (int i=0;i<sentence.Length;i++)
+                {
+                    if ((!char.IsPunctuation(sentence[i]))&& (!char.IsSeparator(sentence[i])))
+                    {
+                        text += sentence[i];
+                    }else
+                    {
+                        if (text != "")
+                        {
+                            k++;
+                            sum += text.Length;
+                            text = "";
+                        }  
+                    }
+                    
+                }
+                Console.WriteLine("Сумма длинн: {0}",sum);
+                Console.WriteLine("Кол-во слов: {0}",k);
+                Console.WriteLine("Средняя длинна слова: {0,6:N1}",sum/k);
+                Console.WriteLine("Для выхода нажмите \'Enter\'...");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
