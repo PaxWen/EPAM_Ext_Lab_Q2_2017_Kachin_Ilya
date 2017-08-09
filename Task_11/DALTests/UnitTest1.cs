e97fc41c23da7062a4629fd84a77dc6a9ca39006
Nikolay Piskarev <nikolay_piskarev@epam.com>
2017-08-09 16:39:21 +0400

    Задание 11 нужно доделать.

--- Task_11/DALTests/UnitTest1.cs
+++ Task_11/DALTests/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace DALTests
     [TestClass]
     public class UnitTest1
     {
-        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Northwind;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";//todo pn нужно в конфиг вынести. ну и не забывай о требовании provider independent стиля (в презентации примеры есть).
         [TestMethod]
         public void TestGetCounts()
         {
@@ -21,38 +21,38 @@ namespace DALTests
         [TestMethod]
         public void TestGetCustOrdersDetail()
         {
-            new HMT_1.DAL().GetCustOrdersDetail(connectionString,10248);
-        }
+            new HMT_1.DAL().GetCustOrdersDetail(connectionString,10248);//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
 
         [TestMethod]
         public void TestSetOrderDate()
         {
-            new HMT_1.DAL().SetOrderDate(connectionString, 10248, @"'04.04.1997'");
-        }
+            new HMT_1.DAL().SetOrderDate(connectionString, 10248, @"'04.04.1997'");//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
         [TestMethod]
         public void TestSetShipedDate()
         {
-            new HMT_1.DAL().SetShipedDate(connectionString, 10248, @"'04.04.1998'");
-        }
+            new HMT_1.DAL().SetShipedDate(connectionString, 10248, @"'04.04.1998'");//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
         [TestMethod]
         public void TestGetOrder()
         {
-            new HMT_1.DAL().GetOrders(connectionString,new int[] {10248,10249 });
-        }
+            new HMT_1.DAL().GetOrders(connectionString,new int[] {10248,10249 });//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
         [TestMethod]
         public void GetDetailedOrder()
         {
-            new HMT_1.DAL().GetDetailedOrder(connectionString, 10248);
-        }
+            new HMT_1.DAL().GetDetailedOrder(connectionString, 10248);//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
         [TestMethod]
         public void TestGetCustOrderHist()
         {
-            new HMT_1.DAL().GetCustOrderHist(connectionString, "ANTON");
-        }
+            new HMT_1.DAL().GetCustOrderHist(connectionString, "ANTON");//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
         [TestMethod]
         public void TestInsert()
         {
-            new DAL().InsertOrder(connectionString,new Order(11111,"ANTON", 6,new DateTime(1998,6,6),new DateTime(1998,9,6),"Nansy 32"));
-        }
+            new DAL().InsertOrder(connectionString,new Order(11111,"ANTON", 6,new DateTime(1998,6,6),new DateTime(1998,9,6),"Nansy 32"));//todo pn очень странный тест, перепиши с использованием AreEqual итп методов
+		}
     }
 }
